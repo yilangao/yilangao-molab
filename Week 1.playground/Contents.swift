afb0605524874bca1c148089756700a5621aa592
yilangao <gylshpt@yahoo.com>
2022-02-04 16:03:21 -0500

    Add week 1 assignment
    Code

---
+++ Week 1.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Garden Rules"
+
+let flw1 = "🥀"
+print(Array(flw1.unicodeScalars))
+
+let flw2 = "🌷"
+print(Array(flw2.unicodeScalars))
+
+let wtr = "💦"
+print(Array(wtr.unicodeScalars))
+
+let Gardening = flw1+"+"+wtr+"="+flw2
+print(Gardening)
